72e1e844805fae45fc356d45f59a918ac5b34f49
Aileneboro <a.borovikova89@gmail.com>
2024-05-02 16:56:36 +0300

    fix

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,10 +1,9 @@
-import ContactList from "../ContactList/ContactList";
-import SearchBox from "../SearchBox/SearchBox";
-import ContactForm from "../ContactForm/ContactForm";
+import ContactList from "../../components/ContactList/ContactList";
+import SearchBox from "../../components/SearchBox/SearchBox";
+import ContactForm from "../../components/ContactForm/ContactForm";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { fetchContacts } from "../../redux/contacts/operations";
-import css from "./App.module.css";
 
 const ContactPage = () => {
   const dispatch = useDispatch();
@@ -14,7 +13,7 @@ const ContactPage = () => {
   }, [dispatch]);
 
   return (
-    <div className={css.container}>
+    <div>
       <h1>Phonebook</h1>
       <ContactForm />
       <SearchBox />
