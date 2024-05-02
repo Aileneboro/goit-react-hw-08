c7d90ca74cdf0889e7a770c89c4317401d54faba
Aileneboro <a.borovikova89@gmail.com>
2024-05-02 14:15:54 +0300

    contacts page

---
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -0,0 +1,26 @@
+import ContactList from "../ContactList/ContactList";
+import SearchBox from "../SearchBox/SearchBox";
+import ContactForm from "../ContactForm/ContactForm";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchContacts } from "../../redux/contacts/operations";
+import css from "./App.module.css";
+
+const ContactPage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <div className={css.container}>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
+};
+
+export default ContactPage;
